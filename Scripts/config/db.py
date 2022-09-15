0216c81d6b0a6b41b014b437b12420b97e2ab5c7
bparedes21 <gestionorganizacion17@gmail.com>
2022-09-15 10:09:53 -0300

    pi-FastApi

--- Scripts/config/db.py
+++ Scripts/config/db.py
@@ -1,5 +1,6 @@
-from sqlalchemy import create_engine, MetaData
-import sqlalchemy
+import pymysql
+import pymysql.cursors
+
 #necesita url usuario, contraseña, servidor donde esta alojado
 # el puerto y la base de datos
 #otras bibliotecas como pymysql
@@ -11,11 +12,46 @@ import sqlalchemy
 # DEFINE THE DATABASE CREDENTIALS
 user = 'root'
 password = 'password'
-host = '127.0.0.1'
-port = 8000
+host = "127.0.0.1"
+port = "8000"
 database = 'dbraces'
 
+#https://programmerclick.com/article/9843531918/
+con = pymysql.connect(host,
+        user,
+        password,
+        database ,
+    )
+                                   
+try:
+# Create a cursor object
+    cursorObject= con.cursor()                                     
+    # SQL query string
+    sqlQuery= "CREATE TABLE Employee(IDArticulo int, Precio int, proveedor varchar(32), nombre varchar(32), precio_pesos int)"  
+    # Execute the sqlQuery
+    cursorObject.execute(sqlQuery)
+    # SQL query string
+    sqlQuery = "show tables"   
+    # Execute the sqlQuery
+    cursorObject.execute(sqlQuery)
+    #Fetch all the rows
+    rows = cursorObject.fetchall()
+    for row in rows:
+        print(row)
 
-engine = create_engine(url="mysql+pymysql://user:password@host:port/database")
-conect=engine.conect()
-meta_data=MetaData()
+except Exception as e:
+
+    print("Exeception occured:{}".format(e))
+
+finally:
+
+    con.close()
+
+#engine = create_engine(url="mysql+pymysql://user:password@host:port/database")
+#conect=engine.conect()
+#meta_data=MetaData()
+
+#users =Table("product",meta_data,column("IDArticulo", Integer),column("Precio", Integer),column("proveedor",string(255)),column("nombre",string(255),column("precio_pesos", Integer)))
+
+#crear tabla
+#meta_data.create_all(engine)
--- Scripts/model/users.py
+++ Scripts/model/users.py
@@ -6,14 +6,16 @@ from sqlalchemy import Table, Column
 from sqlalchemy.sql.sqltypes import Integer, String
 from config.db import engine, meta_data
 
-users =Table("product",meta_data
-,column("IDArticulo", Integer)
-,column("Precio", Integer)
-,column("proveedor",string(255)
-,column("nombre",string(255)
-,column("precio_pesos", Integer)
+#users =Table("product",meta_data
+#,column("IDArticulo", Integer)
+#,column("Precio", Integer)
+#,column("proveedor",string(255)
+#,column("nombre",string(255)
+#,column("precio_pesos", Integer)
 
-)
+#)
 
 #crear tabla
-meta_data.create_all(engine)
+#meta_data.create_all(engine)
+
+#https://www.youtube.com/watch?v=6eVj33l5e9M&ab_channel=FaztCode
